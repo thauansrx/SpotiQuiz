45ef957eff56b1358a3c070f161e249d52cc4788
Thauan <thauantrindade@outlook.com>
2018-11-09 08:33:38 -0200

    Main activity

--- app/src/main/java/aps/com/br/aps_android/MainActivity.java
+++ app/src/main/java/aps/com/br/aps_android/MainActivity.java
@@ -2,12 +2,31 @@ package aps.com.br.aps_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Thauan Trindade on 08/11/2018.
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoIniciar = (Button) findViewById(R.id.buttonIniciarId);
+
     }
+
+    public void iniciar(View view){
+        Intent intent = new Intent(MainActivity.this, TelaQuiz.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
