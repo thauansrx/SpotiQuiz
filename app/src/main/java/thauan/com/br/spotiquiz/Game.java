b7e061856888f62c5b0c0f2e5f14a6727482eff1
Thauan Trindade <thauantrindade@outlook.com>
2018-11-18 18:13:20 -0200

    Update Game.java

--- app/src/main/java/thauan/com/br/spotiquiz/Game.java
+++ app/src/main/java/thauan/com/br/spotiquiz/Game.java
@@ -28,85 +28,85 @@ public class Game extends AppCompatActivity {
     private Button botaoProximo;
 
     String Perguntas[] = {
-            "1. Normalmente, quantos litros de sangue uma pessoa tem?\nEm média quantos são retirados em uma doação de sangue?",
-            "2. De onde é a invenção do chuveiro elétrico?",
-            "3. Qual é o menor e o maior país do mundo?",
-            "4. Qual o nome do ex presidente do Brasil que ficou conhecido como Jango?",
-            "5. Quantas casas decimais tem o número pi?",
-            "6. Atualmente, quantos elementos químicos a tabela periódica possui?",
-            "7. Qual o número mínimo de jogadores em uma partida de futebol?",
-            "8. Quanto tempo a luz do sol demora para chegar na Terra?",
-            "9. Qual personagem folclórico costuma ser agradado pelos caçadores com a oferta de fumo?",
-            "10. Em que período da pré-história o fogo foi descoberto?"
+            "1. A qual música pertence o trecho a seguir? \"É preciso amar as pessoas Como se não houvesse amanhã\"?",
+            "2. Qual a música do U2 que teve o clipe filmado no Rio de Janeiro?",
+            "3. Eu descobri que é azul a cor da parede da casa de Deus... \" é um trecho de qual música da banda Charlie Brown Jr.?",
+            "4. Qual cantor substituiu Dinho Ouro Preto após a sua saída da banda em 1993?",
+            "5. Os Ramones ganharam um Disco de Ouro por causa de um de seus discos. Qual é ele?",
+            "6. \"Eu não tenho carro / Não tenho teto / E se ficar comigo é porque gosta\" é o trecho de qual música?",
+            "7. \"Eu vou fazer um leilão / Quem dá mais pelo meu coração / Me ajude voltar a viver / Eu prefiro que seja você\" é o trecho de uma música cantada por qual dupla?",
+            "8. \"All you need is love / All you need is love / All you need is love, love / Love is all you need\": qual o título desta música e quem a canta?",
+            "9. Qual é o nome do álbum lançado no segundo semestre de 2018 da cantora Anitta?",
+            "10. Qual a playlist que mais fez sucesso no Brasil e no mundo no primeiro semestre de 2018?"
     };
 
     String OpcaoA[] = {
-            "Tem entre 2 à 4 litros. São retirados 450 mililitros.",
-            "França",
-            "Vaticano e Rússia",
-            "Jânio Quadros",
-            "Duas",
-            "113",
-            "8",
-            "12 minutos",
-            "Caipora",
-            "Neolítico"
+            "É preciso",
+            "Elevation",
+            "Só por uma Noite",
+            "Badauí",
+            "Pleasant Dreams",
+            "Construção de Chico Buarque.",
+            "Edson e Hudson",
+            "Everybody needs love - The Rolling Stones",
+            "Solo",
+            "Today's Top Hits"
     };
 
     String OpcaoB[] = {
-            "Tem entre 4 à 6 litros. São retirados 450 mililitros.",
-            "Inglaterra",
-            "Nauru e China",
-            "Jacinto Anjos",
-            "Centenas",
-            "109",
-            "10",
-            "1 dia",
-            "Saci",
-            "Paleolítico"
+            "Sereníssima",
+            "Vertigo",
+            "Não uso sapato",
+            "Cazuza",
+            "Ramones",
+            "Lepo Lepo de Psirico",
+            "Rionegro e Solimões",
+            "Love Story - Taylor Swift.",
+            "Bang",
+            "Top 100 tracks currently on Spotify"
     };
 
     String OpcaoC[] = {
-            "Tem 10 litros. São retirados 2 litros.",
-            "Brasil",
-            "Mônaco e Canadá",
-            "Getúlio Vargas",
-            "Trilhares",
-            "108",
-            "9",
-            "12 horas",
-            "Lobisomem",
-            "Idade dos Metais"
+            "Será",
+            "Gone",
+            "Como Tudo Deve Ser",
+            "Murilo Lima",
+            "Mondo Bizarro",
+            "Se é pra beber, eu bebo de Gusttavo Lima",
+            "Cesar Menotti e Fabiano",
+            "I love you - Paul McCartney.",
+            "Meu Lugar",
+            "Sertanejo"
     };
 
     String OpcaoD[] = {
-            "Tem 7 litros. São retirados 1,5 litros.",
-            "Austrália",
-            "Malta e Estados Unidos",
-            "João Figueiredo",
-            "Vinte",
-            "118",
-            "5",
-            "8 minutos",
-            "Boitatá",
-            "Período da Pedra Polida"
+            "Pais e Filhos",
+            "Magnificent",
+            "Lugar ao Sol",
+            "Chorão",
+            "Adios Amigos",
+            "Tchuco no Tchaco de Parangolé",
+            "Jorge e Matheus",
+            "All you need is love - The Beatles.",
+            "Ritmo Perfeito",
+            "Noite eletrônica"
     };
 
     String OpcaoE [] = {
-            "Tem 0,5 litros. São retirados 0,5 litros",
-            "Itália",
-            "São Marino e índia",
-            "João Goulart",
-            "Milhares",
-            "92",
-            "7",
-            "10 segundos",
-            "Negrinho do Pastoreio",
-            "Idade Média"
+            "Faroeste cabloco",
+            "Walk On",
+            "Longe de você",
+            "João Bosco",
+            "Acid Eaters",
+            "Nenhuma das alternativas",
+            "Fernando e Sorocaba",
+            "Candy Shop - 50cent",
+            "Medicina",
+            "Novidades da semana"
     };
 
     private int[] listaRespostas = new int[Perguntas.length];
-    private int listaGabarito[] = {2,3,1,5,3,4,5,4,1,2};
+    private int listaGabarito[] = {4,5,4,3,3,2,3,4,1,1};
     private int respostasCorretas = 0;
     private int numeroPergunta = 0;
 
